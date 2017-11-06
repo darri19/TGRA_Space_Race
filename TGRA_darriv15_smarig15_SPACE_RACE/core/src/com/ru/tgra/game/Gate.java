8d238d32192ddca1a719519c0fe1b515fd29104e
Smari Bjorn <smarig15@ru.is>
2017-11-06 19:07:43 +0000

    gate color change-ish

--- TGRA_darriv15_smarig15_SPACE_RACE/core/src/com/ru/tgra/game/Gate.java
+++ TGRA_darriv15_smarig15_SPACE_RACE/core/src/com/ru/tgra/game/Gate.java
@@ -1,6 +1,7 @@
 package com.ru.tgra.game;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.ru.tgra.graphics.Color;
 import com.ru.tgra.graphics.Point3D;
 import com.ru.tgra.graphics.shapes.g3djmodel.MeshModel;
 
@@ -12,12 +13,17 @@ public class Gate {
 	Texture tex;
 	float size;
 	
-	public Gate(Point3D pos, float angle, MeshModel model, Texture tex, float size){
+	Color color;
+	int timesTraversed = 0;
+	
+	
+	public Gate(Point3D pos, float angle, MeshModel model, Texture tex, float size, Color color){
 		this.pos = pos;
 		this.angle = angle;
 		this.model = model;
 		this.tex = tex;
 		this.size = size;
+		this.color = color;
 	}
 	
 	public Point3D getPos(){
@@ -34,6 +40,16 @@ public class Gate {
 	
 	public Texture getTex(){
 		return tex;
+	}
+	
+	public boolean traversing(Point3D obj){
+		if(obj.x < pos.x+size && obj.x > pos.x-size &&
+		   obj.z < pos.z+size && obj.z > pos.z-size	){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 	
 }
